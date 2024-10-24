e499a7e331e6ba2fbcd6da2f6ceaf80ef2866d37
Nikbaruch <130352420+Nikbaruch@users.noreply.github.com>
2024-10-24 23:05:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-import { createPublicClient, http } from "viem";
-import { base } from "viem/chains";
-
-const client = createPublicClient({
-  chain: base,
-  transport: http("https://base-mainnet.g.alchemy.com/v2/DawI2TgDeyVcz8cH_OE6WZ6RQvKxPRE5"),
-});
-
-const block = await client.getBlock({
-  blockNumber: 123456n,
-});
-
-console.log(block);
-
 
 
 
